b570120d12f7f0d0818f464109817c9c621374c3
Sascha Turowski <turowski@turtec.de>
2018-06-29 22:38:49 +0200

    changed core

--- App.js
+++ App.js
@@ -11,29 +11,14 @@ import {
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import MainTabNavigator from 'the_core/navigator/MainTabNavigator';
+import {PrimaryButton} from 'the_core/components';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <MainTabNavigator />
     );
   }
 }
