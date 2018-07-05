7bed2fdfbbcdb22511391d09e89c0f26bde80429
Sascha Turowski <turowski@turtec.de>
2018-07-05 14:47:24 +0200

    added redux for internal state handling

--- App.js
+++ App.js
@@ -11,33 +11,66 @@ import {
   Text,
   View
 } from 'react-native';
+
 import MainTabNavigator from 'the_core/navigator/MainTabNavigator';
-import {PrimaryButton} from 'the_core/components';
+//import {autoRehydrate} from 'redux-persist';
+import {compose, createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from 'the_core/redux/reducer';
+
+const store = createStore(reducer,
+  undefined,
+  compose(
+    applyMiddleware(thunk)
+  )
+);
+
+/*const store = createStore(reducers,
+  undefined,
+  compose(
+    applyMiddleware(thunk),
+    autoRehydrate()
+  )
+);*/
+
+//const store = createStore(reducers, applyMiddleware(axiosMiddleware(client)));
+
+
+
+/*export default function configureStore() {
+  store = createStore(reducers,
+    undefined,
+    compose(
+      applyMiddleware(thunk, screenTracking),
+      autoRehydrate()
+    )
+  );
+  return store;
+}*/
+
+
+/**
+* returns the store
+**/
+/*function getStore() {
+  return store;
+}
+
+export {configureStore, getStore};*/
+
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <MainTabNavigator />
+      <Provider store={store} >
+        <MainTabNavigator />
+      </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+//https://github.com/react-navigation/react-navigation/issues/4378
+//https://github.com/facebook/react-native/issues/18201
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('myapp', () => App);
