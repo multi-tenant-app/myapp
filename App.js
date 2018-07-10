c2f2956ba1b2870c45208fe216eab729751821c0
Ethan Nguyen <thanhnx.605@gmail.com>
2018-07-10 15:32:42 +0700

    Refine App.js to follow eslint rules

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from 'react';
 
 import MainTabNavigator from 'the_core/navigator/MainTabNavigator';
-//import {autoRehydrate} from 'redux-persist';
-import {compose, createStore, applyMiddleware } from 'redux';
-import {Provider} from 'react-redux';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from 'the_core/redux/reducer';
 
@@ -26,51 +13,10 @@ const store = createStore(reducer,
   )
 );
 
-/*const store = createStore(reducers,
-  undefined,
-  compose(
-    applyMiddleware(thunk),
-    autoRehydrate()
-  )
-);*/
+const App = () => (
+  <Provider store={store} >
+    <MainTabNavigator />
+  </Provider>
+);
 
-//const store = createStore(reducers, applyMiddleware(axiosMiddleware(client)));
-
-
-
-/*export default function configureStore() {
-  store = createStore(reducers,
-    undefined,
-    compose(
-      applyMiddleware(thunk, screenTracking),
-      autoRehydrate()
-    )
-  );
-  return store;
-}*/
-
-
-/**
-* returns the store
-**/
-/*function getStore() {
-  return store;
-}
-
-export {configureStore, getStore};*/
-
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <Provider store={store} >
-        <MainTabNavigator />
-      </Provider>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-
-});
+export default App;
