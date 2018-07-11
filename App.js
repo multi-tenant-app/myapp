af40dcb5145810a9ed2ef8c1306565197b43a9e1
Ethan Nguyen <thanhnx.605@gmail.com>
2018-07-11 09:45:32 +0700

    Setup experimentals environment

--- App.js
+++ App.js
@@ -3,9 +3,19 @@ import { PersistGate } from 'redux-persist/es/integration/react';
 
 import MainTabNavigator from 'the_core/navigator/MainTabNavigator';
 import { Provider } from 'react-redux';
+
+// Components
+import ExpComponents from 'the_core/uicomponents/ExpComponents';
+
+// Constants
+import { DEBUGS } from 'the_core/config/constants';
+
+// Redux
 import configureStore from 'the_core/redux/store/configureStore';
 
 const App = () => {
+  if (DEBUGS.EXPERIMENTALS) return <ExpComponents />;
+
   const { persistor, store } = configureStore();
 
   return (
